d204b95ee1238fb103eee04836849ae500fed790
Éverson Vieira de Lima <eversonvieiradelima@gmail.com>
2023-07-03 15:48:12 -0300

    Atividades do dia 03/07/2023 - Tquinta

---
+++ atividade1.js
@@ -0,0 +1,38 @@
+// 1)
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(numeros.length);
+console.log(numeros[1]);
+console.log(numeros[3]);
+console.log(numeros[5]);
+console.log(numeros[8]);
+
+// 2)
+console.log("----------------");
+const nomeFilmes = [
+  "A grnade caçada",
+  "O resgate do soldado Ryan",
+  "Busca implácavel",
+  "Até o ultimo homem",
+  "300",
+];
+console.log(nomeFilmes.length);
+console.log(nomeFilmes[2]);
+console.log(nomeFilmes[4]);
+console.log(nomeFilmes[0]);
+
+// 3)
+console.log("----------------");
+const nomeMusicas = [
+  "Ousadia e Alegria",
+  "A Carta",
+  "Pirata e Tesouro",
+  "Pra Você Acreditar",
+  "40 Graus",
+  "Gamei",
+];
+console.log(nomeMusicas.length);
+console.log(nomeMusicas[1]);
+console.log(nomeMusicas[2]);
+console.log(nomeMusicas[6]);
+console.log(nomeMusicas[8]);
+console.log(nomeMusicas[nomeMusicas.length - 1]);
---
+++ atividade2.js
@@ -0,0 +1,52 @@
+// 1)
+const ordenarNumeros = [25, 23, 11, 55, 30, 6, 4, 21, 34, 89, 56];
+ordenarNumeros.sort(); // [11, 21, 23, 25, 30, 34, 4, 55, 56, 6, 89]
+console.log(ordenarNumeros);
+// Observe que 34 vem antes do 4,
+// porque '34' vem antes de '4' em ponto de código Unicode.
+
+// 2)
+console.log("-----------");
+const times = ["Santos", "Sport", "Santa Cruz", "Vasco", "Chapecó"];
+times.push("Flamengo"); // Adicionar o time "Flamengo" no final do array.
+times.shift(); // Remover o time que está no início do array.
+times.unshift("Palmeiras"); // Adicionar o time "Palmeiras" no início do array.
+times.push("Grêmio"); //Adicionar o time "Grêmio" no final do array.
+times.unshift("São Paulo", "Santos"); // Adicionar os times "São Paulo" e "Santos" do início do array.
+console.log(times.length); // Exibir, no console, a quantidade de elementos no array.
+times.pop(); // Remover o time que está no final do array.
+times.sort(); // Exibir, no console, em ordem crescente os times que estão no array.
+console.log(times);
+
+// 3)
+console.log("-----------");
+const atividades = [
+  "Tomar café",
+  "Varrer a casa",
+  "Assistir aula da faculdade",
+  "Listar conteúdo de reúnião",
+  "Almoçar",
+  "Mexer no notebook",
+  "Assistir aula do Start",
+];
+console.log(atividades.reverse()); // Exibir no console as atividades na ordem inversa.
+console.log(atividades[3]); // Selecionar e exibir no console, os elementos nas posições 3 e 6.
+console.log(atividades[6]);
+console.log(atividades.join("-")); // Exibir no console seu array transformado em string. Substituir a vírgula pelo símbolo " - " (hífen).
+const atividadesTop = ["Estudar programação", "Resolver problemas"]; // Criar um array com 2 atividades que você mais gosta e juntar com o array das atividades que você faz durante o dia. E exibir no console o resultado.
+const juncaoAtividades = atividades.concat(atividadesTop);
+console.log(juncaoAtividades);
+
+// 4)
+console.log("-----------");
+const cidades = [
+  "São Lourenço da Mata",
+  "Camaragibe",
+  "Recife",
+  "Olinda",
+  "Garanhuns",
+];
+console.log(cidades.sort()); // Exibir, no console, em ordem crescente os times que estão no array.
+cidades.shift(); // Remover a cidade que está no início do array.
+cidades.pop(); // Remover a cidade que está no final do array.
+console.log(cidades.sort()); // Exibir, no console, em ordem crescente os times que estão no array.
